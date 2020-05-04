4c10bd2f326813bba84e577ba897190fbb719f24
ivan <ivan0699@gmail.com>
2020-05-04 18:43:22 -0300

    Se agrego la busqueda, fijarse en console (en url -> abrir en nueva ventana)

--- search-images/src/componentes/Buscador.js
+++ search-images/src/componentes/Buscador.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import axios from "axios";
+
 class Buscador extends Component{
 
   busquedaRef = React.createRef();
@@ -12,17 +14,17 @@ class Buscador extends Component{
 
   render() {
     return (
-      <form onSubmit= {this.obtenerDatos}>
+      <form onSubmit={this.obtenerDatos}>
         <div className="row">
-          <div className= "form-group col-md-8">
+          <div className="form-group col-md-8">
             <input ref={this.busquedaRef}
             type="text"
             className="form-control form-control-lg"
             placeholder="Busca Tu Imagen. Ejemplo: Futbol" />
           </div>
-          <div className= "form-group col-md-4">
+          <div className="form-group col-md-4">
             <input type="submit"
-            className="btn btn-lg btn-danger btn-block" value = "Buscar..."/>
+            className="btn btn-lg btn-danger btn-block" value= "Buscar..."/>
           </div>
 
         </div>
@@ -37,10 +39,11 @@ class Buscador extends Component{
 class BuscadorForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {value: '', data:''};
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange =this.handleChange.bind(this);
+    this.handleSubmit =this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange(event) {
@@ -52,12 +55,35 @@ class BuscadorForm extends React.Component {
     event.preventDefault();
   }
 
+  handleClick() {
+    console.log("me ejecute ", this.state);
+    console.log("me ejecute ", this.state.value);
+
+
+    const appid = "16379604-fe59dd015569cb8b32af3ce1f";
+    const url =
+      "https://pixabay.com/api/?key=" + appid + "&?q=" +
+      this.state.value + "&image_type=photo";
+
+    axios.get(url).then(res => {
+      this.setState({
+        data: res.data
+      });
+    })
+    .catch(function (error) {
+      alert(error);
+    });
+    console.log("url ", url);
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
           Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="text" value={this.state.value} onChange={this.handleChange}
+          onClick={() => this.handleClick()}
+           />
         </label>
         <input type="submit" value="submit" />
       </form>
